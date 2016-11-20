b52112c5470bf678d682f06921991c0feda03e29
DannyT <dan@moov2.com>
2016-11-20 16:39:40 +0000

    Made enemy eyes follow player

---
+++ Unity/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        // Rotate the camera every frame so it keeps looking at the target 
+        transform.LookAt(target);
+    }
+}
